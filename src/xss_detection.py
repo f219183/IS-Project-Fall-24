26e027e173908e3c3dc1029182e195947e3b34da
Rehan Tariq <157967019+f219183@users.noreply.github.com>
2024-11-12 13:40:55 +0500

    essentials installed

---
+++ src/xss_detection.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from Crypto.Cipher import AES
+import pandas as pd
+import sslscan
+import jinja2
+
+print("All libraries installed successfully!")
