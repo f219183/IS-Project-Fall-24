61abfbe7f1e61c2db9167c9f597144cd58ed67ef
Rehan Tariq <157967019+f219183@users.noreply.github.com>
2024-11-12 21:43:39 +0500

    Revert "essentials installed"
    This reverts commit 26e027e173908e3c3dc1029182e195947e3b34da.

--- src/xss_detection.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from Crypto.Cipher import AES
-import pandas as pd
-import sslscan
-import jinja2
-
-print("All libraries installed successfully!")
